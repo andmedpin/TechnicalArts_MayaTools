23a0b32dd9fcfc9bbf7cf258c60d41d031043881
Andres Medina Pineda <andres.pineda.medina@gmail.com>
2022-03-10 10:17:22 -0800

    Renamed variables to fix Python's style guide

--- Scripts/rock_generator.py
+++ Scripts/rock_generator.py
@@ -2,7 +2,7 @@ import maya.cmds as cmds
 from random import uniform as rand
 import time
 
-currentFrame = 1
+current_frame = 1
 
 #TODO: Ask user for soft deformation or not with a checkbox
 
@@ -10,42 +10,42 @@ cmds.promptDialog(
     title = "Rock Generator",
     message = "How Many Rocks?"
 )
-rockAmmount = int(cmds.promptDialog(q = True, text = True))
+rock_ammount = int(cmds.promptDialog(q = True, text = True))
 
 cmds.promptDialog(
     title = "Name",
     message = "General Name for the Rocks"
 )
-rockName = cmds.promptDialog(q = True, text = True)
+rock_name = cmds.promptDialog(q = True, text = True)
 
-for i in range(1, rockAmmount + 1):
-    randomRadius = rand(1,6)
-    randomTranslateX = rand(-35, 35)
-    randomTranslateZ = rand(-35, 35)
+for i in range(1, rock_ammount + 1):
+    random_radius = rand(1,6)
+    random_translateX = rand(-35, 35)
+    random_translateZ = rand(-35, 35)
     
     cmds.polySphere(
-        radius = randomRadius, 
-        name = rockName + "_%s" %i, 
+        radius = random_radius, 
+        name = rock_name + "_%s" %i, 
         subdivisionsX = 15, 
         subdivisionsY = 15)
     
-    cmds.move(randomTranslateX, 0, randomTranslateZ)
+    cmds.move(random_translateX, 0, random_translateZ)
     
     cmds.ConvertSelectionToFaces()
-    selectedComponents = cmds.ls(sl = True, flatten = True)
+    selected_components = cmds.ls(sl = True, flatten = True)
     
-    for i in range(len(selectedComponents)):
-        cmds.select(selectedComponents[i])
+    for i in range(len(selected_components)):
+        cmds.select(selected_components[i])
         
-        randomModX = rand(-0.5, 0.5)
-        randomModY = rand(-0.5, 0.5)
-        randomModZ = rand(-0.5, 0.5)
+        random_modX = rand(-0.5, 0.5)
+        random_modY = rand(-0.5, 0.5)
+        random_modZ = rand(-0.5, 0.5)
         
-        cmds.move(randomModX,randomModY, randomModZ, relative = True)
+        cmds.move(random_modX,random_modY, random_modZ, relative = True)
     
-    cmds.currentTime(currentFrame)
-    currentFrame += 1
+    cmds.currentTime(current_frame)
+    current_frame += 1
     time.sleep(0.01)
 
-cmds.select(rockName + "_*")
+cmds.select(rock_name + "_*")
 cmds.group(name = "Rocks")
